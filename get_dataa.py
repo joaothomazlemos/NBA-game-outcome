2bc1e2d75577a74d6e54c0532e2897fadd673555
Galadriel-LORD <joaothomazlemos@gmail.com>
2023-01-20 15:13:36 -0300

    trying to get rid of  abug

--- get_dataa.py
+++ get_dataa.py
@@ -3,21 +3,23 @@ import os
 from playwright.sync_api import sync_playwright, TimeoutError as playwrightTimeout
 import time
 
-SEASONS = list(range(2016, 2023))
+#SEASONS = list(range(2016, 2023))
+SEASONS = [2021, 2022]
 
 
 DATA_DIR = 'data2'
 STANDINGS_DIR = os.path.join(DATA_DIR, 'standings') # data2 is a directory where standings will be located inside
 SCORES_DIR = os.path.join(DATA_DIR, 'scores')
 
-def get_html(url, selector, sleep=5, retries=4): # async allow the code after it to imediatelly execute. 
+def get_html(url, selector, sleep=7, retries=3): # async allow the code after it to imediatelly execute. 
     html=None
     for i in range(1, retries+1):
         time.sleep(sleep * i) # each try is longer by a sleep multiplication factor
 
         try:
             with sync_playwright() as p: # istance of playwright object
-                browser = p.firefox.launch() # await will actually wait for the async load of the website complete to lauch the browser
+                browser = p.chromium.launch() # await will actually wait for the async load of the website complete to lauch the browser
+                #context = browser.new_context()
                 page = browser.new_page() # page will be a new tab
                 page.goto(url)
                 print(page.title()) # tracking our webscraping progress of tries and sucess
@@ -27,18 +29,24 @@ def get_html(url, selector, sleep=5, retries=4): # async allow the code after it
             continue # tries again
         else: # if try suceds
             break
-    return html
+    if html != None:
+        return html
+    else:
+        print('Fail')
 
 def scrapy_season(season):
     url  = f"https://www.basketball-reference.com/leagues/NBA_{season}_games.html"
     
     
-    html = get_html(url=url, selector='#content.filter')
+    html = get_html(url=url, selector='#content .filter')
     if html == None:
-        return print('nao deu certo o primeiro htlm')
+        print('still nothing on the first attempt to get the whole page content')
+    else:
+        print('attempt to get the whole page content succeds!')
+
         
 
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'html.parser')
     links = soup.find_all('a')
     href = [l['href'] for l in links] # l is a hyperlink for a month
     stadings_pages = ["https://www.basketball-reference.com{l}" for l in href]
@@ -51,7 +59,9 @@ def scrapy_season(season):
         #grabbing table of month
         html = get_html(url=url, selector='#all_schedule')
         if html == None:
-            return print('nao deu certo o segundo htlm')
+            print(f'still nothing on the first attempt to get the whole {url} content')
+        else:
+            print(f'attempt to get the {url} content succeds!')
         with open (save_path, 'w+') as f:
             f.write(html) # content to be saved in the file  with specified name
     
@@ -60,4 +70,5 @@ def scrapy_season(season):
 #scrapping every season
 for season in SEASONS:
     scrapy_season(season)
+    print(f'scraped season {season} with success')
 
