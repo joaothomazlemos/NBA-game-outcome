13481014226759e9d7e29560d8319c014936ed77
Galadriel-LORD <joaothomazlemos@gmail.com>
2023-04-15 10:20:05 -0300

    ..

--- full_scraping.py
+++ full_scraping.py
@@ -52,7 +52,7 @@ def get_html(url, selector, sleep=7, retries=3): # async allow the code after it
             
             with sync_playwright() as p: # istance of playwright object. You may have to specify the path of the executable file of the browser you want to use.
                 #executable_path= "C:/Users/Usuario/AppData/Local/ms-playwright/firefox-1369/firefox/firefox.exe"
-                browser = p.firefox.launch(executable_path= "C:/Users/Usuario/AppData/Local/ms-playwright/firefox-1369/firefox/firefox.exe") # await will actually wait for the async load of the website complete to lauch the browser
+                browser = p.firefox.launch() # await will actually wait for the async load of the website complete to lauch the browser
                 #context = browser.new_context()
                 page = browser.new_page() # page will be a new tab
                 page.goto(url)
